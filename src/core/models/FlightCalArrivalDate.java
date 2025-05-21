d19a70c8fbe616d33059c73eaad519939349a506
Cristian00077 <cristianviloria.14@gmail.com>
2025-05-21 12:14:20 -0500

    Creacion de clases de single responsibility

---
+++ src/core/models/FlightCalArrivalDate.java
@@ -0,0 +1,14 @@
+
+package core.models;
+
+public class FlightCalArrivalDate {
+    private Flight flight;
+
+    public FlightCalArrivalDate(Flight flight) {
+        this.flight = flight;
+    }
+    
+    public void calculateArrivalDate(){
+        
+    }
+}
---
+++ src/core/models/PassengerAge.java
@@ -0,0 +1,14 @@
+
+package core.models;
+
+public class PassengerAge {
+    private Passenger passenger;
+
+    public PassengerAge(Passenger passenger) {
+        this.passenger = passenger;
+    }
+    
+    public void calculateAge(){
+        
+    }
+}
